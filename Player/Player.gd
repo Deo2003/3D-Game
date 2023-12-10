e0d4c666b44427e3b2c1b611920684189c5d6424
Deo2003 <48349866+Deo2003@users.noreply.github.com>
2023-12-10 11:50:57 -0500

    Updates

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,6 +4,7 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const MOUSE_SENSITIVITY = 0.001
+var health = 5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -32,6 +33,8 @@ func _physics_process(delta):
 		for w in weapons.get_children():
 			if w.has_method("shoot"):
 				w.shoot()
+	
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
